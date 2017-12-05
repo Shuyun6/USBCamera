403dca127d55d458c4901d21c47cdccb7fb0e977
Shuyun <shuyungogogo@hotmail.com>
2017-12-05 17:27:04 +0800

    commite

---
+++ app/src/main/java/shuyun/usbcamera/GLHelper.java
@@ -0,0 +1,227 @@
+package shuyun.usbcamera;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.SurfaceTexture;
+import android.opengl.GLES20;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
+import static android.opengl.EGL14.EGL_CONTEXT_CLIENT_VERSION;
+import static android.opengl.EGL14.EGL_OPENGL_ES2_BIT;
+import static android.opengl.GLES20.GL_CLAMP_TO_EDGE;
+import static android.opengl.GLES20.GL_COLOR_ATTACHMENT0;
+import static android.opengl.GLES20.GL_DEPTH_ATTACHMENT;
+import static android.opengl.GLES20.GL_DEPTH_COMPONENT16;
+import static android.opengl.GLES20.GL_FRAMEBUFFER;
+import static android.opengl.GLES20.GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT;
+import static android.opengl.GLES20.GL_RENDERBUFFER;
+import static android.opengl.GLES20.GL_RGB;
+import static android.opengl.GLES20.GL_TEXTURE_2D;
+import static android.opengl.GLES20.GL_TEXTURE_WRAP_S;
+import static android.opengl.GLES20.GL_TEXTURE_WRAP_T;
+import static android.opengl.GLES20.GL_UNSIGNED_BYTE;
+import static android.opengl.GLES20.glBindFramebuffer;
+import static android.opengl.GLES20.glBindRenderbuffer;
+import static android.opengl.GLES20.glBindTexture;
+import static android.opengl.GLES20.glCheckFramebufferStatus;
+import static android.opengl.GLES20.glFramebufferRenderbuffer;
+import static android.opengl.GLES20.glFramebufferTexture2D;
+import static android.opengl.GLES20.glGenFramebuffers;
+import static android.opengl.GLES20.glGenRenderbuffers;
+import static android.opengl.GLES20.glGenTextures;
+import static android.opengl.GLES20.glGetError;
+import static android.opengl.GLES20.glRenderbufferStorage;
+import static android.opengl.GLES20.glTexImage2D;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by Shuyun on 2017/12/5 0005.
+ */
+
+public class GLHelper {
+
+    private EGLDisplay eglDisplay;
+    private EGLSurface eglSurface;
+    private EGLContext eglContext;
+    private EGL10 egl10;
+    private Context context;
+    private int texture2Framebuffer;
+    private int width, height;
+    private int frameBufferId, renderBufferId;
+
+    public GLHelper(Context context){
+        this.context = context;
+    }
+
+    public void setDisplaySize(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public void initEGL(SurfaceTexture texture) {
+        egl10 = (EGL10) EGLContext.getEGL();
+        eglDisplay = egl10.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        if (eglDisplay == EGL10.EGL_NO_DISPLAY) {
+            throw new RuntimeException("eglGetDisplay failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+        int[] version = new int[2];
+        if (!egl10.eglInitialize(eglDisplay, version)) {
+            throw new RuntimeException("eglInitialize failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+        int[] configsCount = new int[1];
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] configSpec = {
+                EGL10.EGL_RENDERABLE_TYPE,
+                EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_ALPHA_SIZE, 8,
+                EGL10.EGL_DEPTH_SIZE, 0,
+                EGL10.EGL_STENCIL_SIZE, 0,
+                EGL10.EGL_NONE
+        };
+        EGLConfig eglConfig = null;
+        if (!egl10.eglChooseConfig(eglDisplay, configSpec, configs, 1, configsCount)) {
+            throw new IllegalArgumentException("eglChooseConfig failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        } else if (configsCount[0] > 0) {
+            eglConfig = configs[0];
+        }
+        if (eglConfig == null) {
+            throw new RuntimeException("eglConfig not initialized");
+        }
+
+        int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};
+        eglContext = egl10.eglCreateContext(eglDisplay, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
+        eglSurface = egl10.eglCreateWindowSurface(eglDisplay, eglConfig, texture, null);
+        if (eglSurface == null || eglSurface == EGL10.EGL_NO_SURFACE) {
+            int error = egl10.eglGetError();
+            if (error == EGL10.EGL_BAD_NATIVE_WINDOW) {
+                Log.e("test", "eglCreateWindowSurface returned EGL10.EGL_BAD_NATIVE_WINDOW");
+                return;
+            }
+            throw new RuntimeException("eglCreateWindowSurface failed " +
+                    android.opengl.GLUtils.getEGLErrorString(error));
+        }
+        if (!egl10.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            throw new RuntimeException("eglMakeCurrent failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+    }
+
+    public void setupFrameBuffer(){
+        texture2Framebuffer = genTexture();
+        glBindTexture(GL_TEXTURE_2D, texture2Framebuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, null);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        int[] buffer = new int[1];
+        glGenFramebuffers(1, buffer, 0);
+        frameBufferId = buffer[0];
+        glBindFramebuffer(GL_FRAMEBUFFER, frameBufferId);
+        int[] buffer2 = new int[1];
+        glGenRenderbuffers(1, buffer2, 0);
+        renderBufferId = buffer2[0];
+        glBindRenderbuffer(GL_RENDERBUFFER, renderBufferId);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+        unBindFrameBuffer();
+    }
+
+    public void bindFrameBuffer(){
+        glViewport(0, 0, width, height);
+        glBindFramebuffer(GL_FRAMEBUFFER, frameBufferId);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture2Framebuffer, 0);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferId);
+    }
+
+    public void unBindFrameBuffer(){
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+    public void swapBuffer(){
+        egl10.eglSwapBuffers(eglDisplay, eglSurface);
+    }
+
+    public int genTexture(){
+        int[] texture = new int[1];
+        glGenTextures(1, texture, 0);
+        return texture[0];
+    }
+
+    public int linkProgram(int vertextShaderResource, int fragmentShaderResource){
+        int vertextId = compileVertextShader(readResource(vertextShaderResource));
+        int fragmentId = compileFragmentShader(readResource(vertextShaderResource));
+        int program = linkProgram(vertextId, fragmentId);
+        return program;
+    }
+
+    public int compileVertextShader(String shaderCode){
+        return compileShader(GLES20.GL_VERTEX_SHADER, shaderCode);
+    }
+
+    public int compileFragmentShader(String shaderCode){
+        return compileShader(GLES20.GL_FRAGMENT_SHADER, shaderCode);
+    }
+
+    private int compileShader(int type, String shaderCode){
+        int shaderObjectid = GLES20.glCreateShader(type);
+        if(shaderObjectid == 0){
+            return shaderObjectid;
+        }
+        GLES20.glShaderSource(shaderObjectid, shaderCode);
+        GLES20.glCompileShader(shaderObjectid);
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shaderObjectid, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+        Log.e("test", "compileStatus[0] "+compileStatus[0]);
+        if(compileStatus[0] == 0){
+            GLES20.glDeleteShader(shaderObjectid);
+            Log.e("test", GLES20.glGetShaderInfoLog(shaderObjectid));
+            return 0;
+        }
+        return shaderObjectid;
+    }
+
+    private String readResource(int resourceId) {
+        StringBuilder body = new StringBuilder();
+        try {
+            InputStream inputStream = context.getResources().openRawResource(resourceId);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String nextLine;
+            while ((nextLine = bufferedReader.readLine()) != null) {
+                body.append(nextLine);
+                body.append('\n');
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Could not open resource: " + resourceId, e);
+        } catch (Resources.NotFoundException nfe) {
+            throw new RuntimeException("Resource not found: " + resourceId, nfe);
+        }
+        return body.toString();
+    }
+
+    public int getTexture2Framebuffer() {
+        return texture2Framebuffer;
+    }
+}
