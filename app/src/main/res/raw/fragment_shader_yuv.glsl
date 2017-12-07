b4f1b11b736c6ed331d65267b7b9004867735cc3
Shuyun <shuyungogogo@hotmail.com>
2017-12-07 10:49:52 +0800

    Commit

--- app/src/main/res/raw/fragment_shader_yuv.glsl
+++ app/src/main/res/raw/fragment_shader_yuv.glsl
@@ -6,12 +6,13 @@ varying vec2 vTextureCoord;
 void main() {
 
     vec3 yuv = vec3(
-            texture2D(u_TextureY, vTextureCoord).r - 0.0625,
-            texture2D(u_TextureUV, vTextureCoord).a - 0.5,
-            texture2D(u_TextureUV, vTextureCoord).r - 0.5
-        );
-    vec3 rgb = mat3( 1,       1,         1,
-                       0,       -0.39465,  2.03211,
-                       1.13983, -0.58060,  0) * yuv;
+        texture2D(u_TextureY, vTextureCoord).r - 0.0625,
+        texture2D(u_TextureUV, vTextureCoord).a - 0.5,
+        texture2D(u_TextureUV, vTextureCoord).r - 0.5
+    );
+    vec3 rgb = mat3( 1,           1,         1,
+                      0,    -0.39465,   2.03211,
+                1.13983,    -0.58060,         0) * yuv;
     gl_FragColor = vec4(rgb, 1);
+//    gl_FragColor = texture2D(u_TextureY, vTextureCoord);
 }
